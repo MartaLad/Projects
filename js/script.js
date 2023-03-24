b0f919336fb0f55309a84977beb0147ee014dd78
Marta Ladutska <marta.lad@mail.ru>
2023-03-24 10:46:57 +0300

    Projects

---
+++ js/script.js
@@ -0,0 +1,68 @@
+/*const container = document.querySelector(".container")*/
+
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  a11y: {
+    paginationBulletMessage: 'Перейти к слайду {{index}}'
+  },
+});
+
+let tabsBtn = document.querySelectorAll('.tabs-nav__btn');
+let tabsItem = document.querySelectorAll('.tabs-item');
+
+tabsBtn.forEach(function (element) {
+  element.addEventListener('click', function (e) {
+    const path = e.currentTarget.dataset.path;
+
+    tabsBtn.forEach(function (btn) { btn.classList.remove('tabs-nav__btn--active') });
+    e.currentTarget.classList.add('tabs-nav__btn--active');
+
+    tabsItem.forEach(function (element) { element.classList.remove('tabs-item--active') });
+    document.querySelector(`[data-target="${path}"]`).classList.add('tabs-item--active');
+  })
+})
+
+new Accordion('.accordion');
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  document.getElementById('open-search-form').addEventListener('click', (e) => {
+    document.getElementById('search-form').classList.add('search-form_show')
+  })
+
+  document.getElementById('search-form-close').addEventListener('click', (e) => {
+    document.getElementById('search-form').classList.remove('search-form_show')
+  })
+
+  document.getElementById('search-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
+
+})
+
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.header__link');
+
+burger.addEventListener('click',
+  function () {
+    burger.classList.toggle('burger--active');
+    menu.classList.toggle('header__nav--active');
+    document.body.classList.toggle('stop-scroll');
+  })
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function () {
+    burger.classList.remove('burger--active');
+    menu.classList.remove('header__nav--active');
+    document.body.classList.remove('stop-scroll');
+  })
+})
